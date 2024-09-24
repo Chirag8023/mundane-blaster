cd2a13c322f3f87d6464f9afc7e5fd8b54df629d
Cʜɪʀᴀɢ Aʀᴏʀᴀ <chirag8023@gmail.com>
2024-09-24 13:51:29 +0530

    beam error resolve

--- beam.js
+++ beam.js
@@ -1,17 +1,20 @@
 class Beam extends Phaser.GameObjects.Sprite {
     constructor(scene) {
         var x = scene.player.x;
-        var y = scene.player.y;
+        var y = scene.player.y-16;
         super(scene, x, y, "beam");
         scene.add.existing(this);
-        scene.projectiles.add(this);
+
         this.play("beam_anim");
         scene.physics.world.enableBody(this);
         this.body.velocity.y = -250;
+
+        scene.projectiles.add(this);
+
     }
 
     update() {
-        if (this.y < 32) {
+        if (this.y < 0) {
             this.destroy();
         }
     }
