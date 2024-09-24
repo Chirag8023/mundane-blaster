67b472eac1149f0fba5d89b742487540f1d4cc82
Cʜɪʀᴀɢ Aʀᴏʀᴀ <chirag8023@gmail.com>
2024-09-24 15:07:59 +0530

    sound effects added

--- Scene2.js
+++ Scene2.js
@@ -79,6 +79,22 @@ class Scene2 extends Phaser.Scene {
 
         var scoreFormated = this.zeroPad(this.score, 6);
         this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "SCORE " + scoreFormated, 16);
+
+        this.beamSound = this.sound.add("audio_beam");
+        this.explosionSound = this.sound.add("audio_explosion");
+        this.pickupSound = this.sound.add("audio_pickup");
+
+        this.music = this.sound.add("music");
+        var musicConfig = {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: true,
+            delay: 0
+        }
+        this.music.play(musicConfig);
     }
 
     update() {
@@ -136,11 +152,13 @@ class Scene2 extends Phaser.Scene {
     }
 
     shootBeam() {
-        var beam = new Beam(this);        
+        var beam = new Beam(this);
+        this.beamSound.play();
     }
 
     pickPowerUp(player, powerUp) {
         powerUp.disableBody(true, true);
+        this.pickupSound.play();
     }
 
     hurtPlayer(player, enemy) {
@@ -168,6 +186,7 @@ class Scene2 extends Phaser.Scene {
         this.score += 15;
         var scoreFormated = this.zeroPad(this.score, 6);
         this.scoreLabel.text = "SCORE " + scoreFormated;
+        this.explosionSound.play();
     }
 
     zeroPad(number, size) {
